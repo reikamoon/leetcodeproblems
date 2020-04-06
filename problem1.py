e0a4b5f667510ae264c05bf790063c2c010c32ae
Anjelica <ambrosio.anjelica@gmail.com>
2020-04-06 10:39:03 -0700

    first commit

---
+++ problem1.py
@@ -0,0 +1,3 @@
+# Problem 1: Given a 32 bit signed integer, reverse digits of an integer.
+# Restate the problem: Reverse the digits in the integer to get a new number.
+# Questions: How many digits are there?
